b26ed207d792090a9d90062e3bbe7d0f3debdb18
SWoto <brunostinghel137@gmail.com>
2023-06-06 15:30:13 -0300

    move fill_roles outside debug

--- app_users.py
+++ app_users.py
@@ -18,14 +18,14 @@ if __name__ == "__main__":
             def create_tables():
                 db.create_all()
 
-            @app.before_first_request
-            def fill_roles():
-                role_user = {"name": "user"}
-                role_admin = {"name": "driver"}
-                if not UserRoleModel.find_by_name(**role_user):
-                    db.session.add(UserRoleModel(**role_user))
-                if not UserRoleModel.find_by_name(**role_admin):
-                    db.session.add(UserRoleModel(**role_admin))
-                db.session.commit()
+    @app.before_first_request   
+    def fill_roles():
+        role_user = {"name": "user"}
+        role_admin = {"name": "driver"}
+        if not UserRoleModel.find_by_name(**role_user):
+            db.session.add(UserRoleModel(**role_user))
+        if not UserRoleModel.find_by_name(**role_admin):
+            db.session.add(UserRoleModel(**role_admin))
+        db.session.commit()
 
     app.run(host="0.0.0.0", port=os.getenv("USERS_API_PORT"))
